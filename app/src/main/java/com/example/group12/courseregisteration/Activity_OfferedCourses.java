6c3ea42826cd6c60bcf709a17f3b43ece62548c2
Owner <j94679@gmail.com>
2018-07-09 00:59:34 -0300

    Search Bar Creation
    by
    
    Peter and Mao

--- app/src/main/java/com/example/group12/courseregisteration/Activity_OfferedCourses.java
+++ app/src/main/java/com/example/group12/courseregisteration/Activity_OfferedCourses.java
@@ -1,8 +1,12 @@
 package com.example.group12.courseregisteration;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +19,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Gets the list of courses from the database, and displays them here on a spinnner.
@@ -23,6 +31,8 @@ import com.google.firebase.database.ValueEventListener;
 public class Activity_OfferedCourses extends AppCompatActivity {
 
     private Button buttonBack;
+    MaterialSearchView searchView;
+    String[] courseName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +56,10 @@ public class Activity_OfferedCourses extends AppCompatActivity {
                     position++;
                 }
 
+                courseName = courseNames;
+
                 displayCourses(courseNames);
+
 
             }
             @Override
@@ -55,6 +68,56 @@ public class Activity_OfferedCourses extends AppCompatActivity {
             }
         });
 
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Course Search");
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
+
+        searchView = (MaterialSearchView)findViewById(R.id.search_view);
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                displayCourses(courseName);
+            }
+        });
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText != null && !newText.isEmpty())
+                {
+                    List<String> IsFound = new ArrayList<String>();
+                    for (String s : courseName)
+                    {
+                        if(s.toLowerCase().contains(newText))
+                        {
+                            IsFound.add(s);
+                        }
+                    }
+
+                    String[] stockArr = new String[IsFound.size()];
+                    stockArr = IsFound.toArray(stockArr);
+                    displayCourses(stockArr);
+                }
+                else
+                {
+                    displayCourses(courseName);
+                }
+
+                return true;
+            }
+        });
 
         //back button to user profile
         buttonBack = (Button) findViewById(R.id.buttonBack);
@@ -95,6 +158,15 @@ public class Activity_OfferedCourses extends AppCompatActivity {
                 }
         );
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
     }
 
 
